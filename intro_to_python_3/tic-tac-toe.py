c3d3e40c801aad92675d47411c9d47e59ec698e6
Chukwuka1488 <juniorakibor@yahoo.com>
2021-02-08 19:45:03 +0100

    to define a winner

--- intro_to_python_3/tic-tac-toe.py
+++ intro_to_python_3/tic-tac-toe.py
@@ -6,27 +6,39 @@ game = [[0, 0, 0],
         [0, 0, 0]]
 
 
+def win(current_game):
+    for row in game:
+        print(row)
+        col1 = row[0]
+        col2 = row[1]
+        col3 = row[2]
+
+        if col1 == col2 == col3:
+            print("You won!!!")
+
+win(game)
+
 # functions use when we want a repetitive result from our program
 
-def game_board(game_map, player=0, row=0, column=0, just_display=False):
-    try:
-        print("   a  b  c")  # column
-        if not just_display:
-            game_map[row][column] = player
-        for count, row in enumerate(game_map):  # Built-in functions using enumerate: returns the index and the value
-            print(count, row)
-        return game_map
-    except IndexError as e:
-        print("Error: make sure you input row/column as 0, 1 or 2?", e)
-
-    except  Exception as e:
-        print("something bad just happened!", e)
-
-
-game = game_board(game, just_display=True)
-game = game_board(game_board, player=1, row=2, column=0)
-
-# indexing and slicing
-# game[0][1] = 1
-#
-# game_board()
+# def game_board(game_map, player=0, row=0, column=0, just_display=False):
+#     try:
+#         print("   a  b  c")  # column
+#         if not just_display:
+#             game_map[row][column] = player
+#         for count, row in enumerate(game_map):  # Built-in functions using enumerate: returns the index and the value
+#             print(count, row)
+#         return game_map
+#     except IndexError as e:
+#         print("Error: make sure you input row/column as 0, 1 or 2?", e)
+# 
+#     except  Exception as e:
+#         print("something bad just happened!", e)
+# 
+# 
+# game = game_board(game, just_display=True)
+# game = game_board(game_board, player=1, row=2, column=0)
+# 
+# # indexing and slicing
+# # game[0][1] = 1
+# #
+# # game_board()
