27f7748c76ff101299582a6eb6327e5369670bab
Chukwuka1488 <juniorakibor@yahoo.com>
2021-02-08 19:11:06 +0100

    adding game_map as a placeholder, a name, a variable for the actual list value you are passing to the game_board function

--- intro_to_python_3/tic-tac-toe.py
+++ intro_to_python_3/tic-tac-toe.py
@@ -8,19 +8,21 @@ game = [[0, 0, 0],
 
 # functions use when we want a repetitive result from our program
 
-def game_board(player=0, row=0, column=0, just_display=False):
+def game_board(game_map, player=0, row=0, column=0, just_display=False):
     # column
     print("   a  b  c")
     if not just_display:
-        game[row][column] = player
+        game_map[row][column] = player
     # Built-in functions using enumerate: returns the index and the value
-    for count, row in enumerate(game):
+    for count, row in enumerate(game_map):
         print(count, row)
+
+        return game_map
         # count += 1
         # for item in row:
         #     print(item)
-game_board(just_display=True)
-game_board(player=1, row=2, column=0)
+game = game_board(game, just_display=True)
+game = game_board(game, player=1, row=2, column=0)
 
 # indexing and slicing
 # game[0][1] = 1
