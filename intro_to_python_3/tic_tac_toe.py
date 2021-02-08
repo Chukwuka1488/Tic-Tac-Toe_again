8561f175cd22094e15da84270c0cb779d141fe38
Chukwuka1488 <juniorakibor@yahoo.com>
2021-02-08 23:43:33 +0100

    game modified with print statement and all_same function added

--- intro_to_python_3/tic_tac_toe.py
+++ intro_to_python_3/tic_tac_toe.py
@@ -1,31 +1,44 @@
 import itertools
 
+
 def win(current_game):
+    def all_same(l):
+        if l.count(l[0]) == len(l) and l[0] != 0:
+            return True
+        else:
+            return False
+
     # horizontal winner
     for row in game:
-        if row.count(row[0]) == len(row) and row[0] != 0:
+        if all_same(row):
             print(f"Player {row[0]} is the winner horizontally!")
+            return True
 
     # diagonals
     diags = []
     for col, row in enumerate(reversed(range(len(game)))):
         diags.append(game[row][col])
-    if diags.count(diags[0]) == len(diags) and diags[0] != 0:
+    if all_same(diags):
         print(f"Player {diags[0]} is the winner diagonally upwards - left to right!")
+        return True
 
     diags = []
     for idx in range(len(game)):
         diags.append(game[idx][idx])
-    if diags.count(diags[0]) == len(diags) and diags[0] != 0:
+    if all_same(diags):
         print(f"Player {diags[0]} is the winner diagonally downwards - left to right!")
+        return True
 
     # verticals
     for col in range(len(game)):
         check = []
         for row in game:
             check.append(row[col])
-        if check.count(check[0]) == len(check) and check[0] != 0:
+        if all_same(check):
             print(f"Player {check[0]} is the winner vertically!")
+            return True
+
+    return False
 
 
 def game_board(game_map, player=0, row=0, column=0, just_display=False):
@@ -37,7 +50,7 @@ def game_board(game_map, player=0, row=0, column=0, just_display=False):
         print("   0  1  2")  # column
         if not just_display:
             game_map[row][column] = player
-        for count, row in enumerate(game_map):  #enumerate: returns the index and the value
+        for count, row in enumerate(game_map):  # enumerate: returns the index and the value
             print(count, row)
         return game_map, True
 
@@ -59,14 +72,16 @@ while play:
 
     game_won = False
     game, _ = game_board(game, just_display=True)
+
     player_choice = itertools.cycle([1, 2])
     while not game_won:
+        print()
         current_player = next(player_choice)
-        player_choice = itertools.cycle([1, 2])
         print(f"Current Player: {current_player}")
         played = False
 
         while not played:
             column_choice = int(input("What column do you want to play? (0, 1, 2): "))
             row_choice = int(input("What row do you want to play? (0, 1, 2): "))
+            print()
             game, played = game_board(game, current_player, row_choice, column_choice)
