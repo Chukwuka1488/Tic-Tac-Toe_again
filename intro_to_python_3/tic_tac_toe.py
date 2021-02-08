c47faf0da9e2d7aa10378b5ad44c7071e1168235
Chukwuka1488 <juniorakibor@yahoo.com>
2021-02-08 23:25:37 +0100

    correction for overlapping plays

--- intro_to_python_3/tic_tac_toe.py
+++ intro_to_python_3/tic_tac_toe.py
@@ -30,16 +30,24 @@ def win(current_game):
 
 def game_board(game_map, player=0, row=0, column=0, just_display=False):
     try:
-        print("   a  b  c")  # column
+        if game_map[row][column] != 0:
+            print("This position is taken! Choose another!")
+            print()
+            return game_map, False
+        print("   0  1  2")  # column
         if not just_display:
             game_map[row][column] = player
         for count, row in enumerate(game_map):  #enumerate: returns the index and the value
             print(count, row)
-        return game_map
+        return game_map, True
+
     except IndexError as e:
         print("Error: make sure you input row/column as 0, 1 or 2?", e)
+        return game_map, False
+
     except  Exception as e:
         print("something bad just happened!", e)
+        return game_map, False
 
 
 play = True
@@ -50,10 +58,15 @@ while play:
             [0, 0, 0]]
 
     game_won = False
-    game = game_board(game, just_display=True)
+    game, _ = game_board(game, just_display=True)
     player_choice = itertools.cycle([1, 2])
     while not game_won:
         current_player = next(player_choice)
-        column_choice = int(input("What column do you want to play? (0, 1, 2): "))
-        row_choice = int(input("What row do you want to play? (0, 1, 2): "))
-        game = game_board(game, current_player, row_choice, column_choice)
+        player_choice = itertools.cycle([1, 2])
+        print(f"Current Player: {current_player}")
+        played = False
+
+        while not played:
+            column_choice = int(input("What column do you want to play? (0, 1, 2): "))
+            row_choice = int(input("What row do you want to play? (0, 1, 2): "))
+            game, played = game_board(game, current_player, row_choice, column_choice)
